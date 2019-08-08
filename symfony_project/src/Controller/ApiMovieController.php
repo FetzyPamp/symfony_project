16bd41af9cfc46f4c39f310c76425dfce7d98ee4
Jonathan Lamac <Jonathan.Lamac@coding-academy.fr>
2019-08-08 20:43:53 +0200

    added menu bar frontside and it's WORKING

--- symfony_project/src/Controller/ApiMovieController.php
+++ symfony_project/src/Controller/ApiMovieController.php
@@ -16,11 +16,13 @@ class ApiMovieController extends AbstractController
         
         $movies = file_get_contents('https://api.themoviedb.org/3/movie/popular?api_key=fe9e318b04bec15f80e7ddf05a462e39');
         $moviesTab = $serializer->decode($movies, 'json');
+        $category=file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=fe9e318b04bec15f80e7ddf05a462e39');
+        $categoryTab=$serializer->decode($category, 'json');
         // $mesRegionsObjet = $serializer->denormalize($films,'App\Entity\Region[]');
         // dump($moviesTab);
         // die();
         return $this->render('api_movie/index.html.twig', [
-            'movies' => $moviesTab
+            'movies' => $moviesTab, 'genres'=> $categoryTab
             
         ]);
     }
