6f2e192f9835858952dc7a713aeaffb2db9f4dfe
Jonathan Lamac <Jonathan.Lamac@coding-academy.fr>
2019-08-11 14:29:30 +0200

    fixed nav path

--- symfony_project/src/Controller/UsersController.php
+++ symfony_project/src/Controller/UsersController.php
@@ -91,4 +91,9 @@ class UsersController extends AbstractController
 
         return $this->redirectToRoute('users_index');
     }
+
+    // public function form(Users $user, Request $request, ObjectManager $manager)
+    // {
+    //     $user = new Users
+    // }
 }
